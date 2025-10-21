a4365f38d5a688ccad236b691a2154450e69a3e2
Craig Sloggett <craigsloggett@gmail.com>
2025-10-21 12:21:36 -0400

    Update backend configuration

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,3 @@
 module "terraform_fabric_data_engineering_onboarding" {
-  source = "git::https://github.com/craigsloggett-lab/terraform-fabric-data-engineering-onboarding.git?ref=add-fabric-resources"
+  source = "../"
 }
